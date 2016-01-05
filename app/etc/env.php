f03cc25f0ccfd62a630670cff6bee27feb648be7
root <root@server.infinite-techsolutions.com>
2016-01-05 13:38:42 -0500

    Added all files needed to replicate environment

---
+++ app/etc/env.php
@@ -0,0 +1,61 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_g7d4',
+  ),
+  'install' => 
+  array (
+    'date' => 'Wed, 30 Dec 2015 15:30:14 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'be470fb3a4d70199d65514432c18933c',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magentod_1',
+        'username' => 'magentod_1_user',
+        'password' => 'M12!#8$#T$EN',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+);
